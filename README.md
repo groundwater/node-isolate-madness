0200d180549a8b4f26fe75b1fd5f9b4fff3c17c2
Jacob Groundwater <groundwater@gmail.com>
2014-05-25 09:47:32 +1000

    add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# isolate madness
+
+Experiments with running code in other isolates,
+and in other threads.
+
+## usage
+
+*requires node 0.11.x*
+
+```
+$ node demo.js
+```
--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ var iso = require('./build/Release/binding.node')
 var child = require('fs').readFileSync('child.js')
 
 var start = Date.now()
-iso.demo(child, 324, function(res){
+iso.demo(child, 322312144, function(res){
   console.log('\nTime:', Date.now() - start)
   console.log(res)
 })
